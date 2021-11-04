3d01eddc87d8934903820e21b0200442eb61985e
eppic <42480669+eppic@users.noreply.github.com>
2021-11-04 13:11:01 +0100

    Update QuietCMD.bat

--- QuietCMD.bat
+++ QuietCMD.bat
@@ -1,29 +1,34 @@
 @echo off
-title Quiet CMD
+title QuietCMD by eppic
 cls
-echo Quiet CMD - Mute Windows Copyright Information on CMD.
+echo QuietCMD - Mute Windows Copyright Information on CMD.
 echo.
 echo This Script hides the copyright information everytime cmd.exe starts by
-echo setting CMD's Autorun to "init.cmd" which contains a "cls" command.
+echo setting CMD's AutoRun to "%APPDATA%\QuietCMD\init.cmd" which contains a "cls" command.
 echo.
 echo If something went wrong, delete the "AutoRun" Key in the Registry under
 echo. "HKEY_CURRENT_USER\Software\Microsoft\Command Processor"
 echo.
 pause
-echo.
+echo. 
 
 echo Adding Key to Registry...
 reg add "HKCU\Software\Microsoft\Command Processor" /v AutoRun ^
-  /t REG_EXPAND_SZ /d "init.cmd" /f
+  /t REG_EXPAND_SZ /d "%APPDATA%\QuietCMD\init.cmd" /f
 echo.
 
 echo Copying init.cmd...
-copy nul init.cmd
+mkdir %APPDATA%\QuietCMD\
+copy nul %APPDATA%\QuietCMD\init.cmd
 echo.
 
 echo Writing to init.cmd...
-echo @echo off> %USERPROFILE%\init.cmd
-echo cls>> %USERPROFILE%\init.cmd
+echo @echo off> %APPDATA%\QuietCMD\init.cmd
+echo cls>> %APPDATA%\QuietCMD\init.cmd
+echo.
+
+echo Finishing...
+echo start "" "https://github.com/eppic/QuietCMD"> %APPDATA%\QuietCMD\Open-Repo.bat
 echo.
 
 echo Finished.
