18aad0d3a5b7d97d194221cb1ecb5a55875a5545
eppic <42480669+eppic@users.noreply.github.com>
2021-11-04 13:10:40 +0100

    Add files via upload

---
+++ QC-No-Reg.bat
@@ -0,0 +1,22 @@
+@echo off
+title QuietCMD by eppic - No Registry installer
+cls
+echo QuietCMD - Mute Windows Copyright Information on CMD.
+echo.
+echo No Registry Edition
+echo.
+pause
+echo.
+
+echo Copying init.cmd...
+mkdir %APPDATA%\QuietCMD\
+copy nul %APPDATA%\QuietCMD\init.cmd
+echo.
+
+echo Writing to init.cmd...
+echo @echo off> %APPDATA%\QuietCMD\init.cmd
+echo cls>> %APPDATA%\QuietCMD\init.cmd
+echo.
+
+echo Finished.
+pause
