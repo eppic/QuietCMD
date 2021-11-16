56c8c98ad55441057ac01cffb10432e1050f2ca7
eppic <ncraft121@gmail.com>
2021-11-16 16:31:29 +0100

    add uninstall

---
+++ QC-Uninstall.bat
@@ -0,0 +1,21 @@
+@echo off
+title QuietCMD by eppic - Uninstaller
+cls
+echo QuietCMD - Mute Windows Copyright Information on CMD.
+echo.
+echo Run this Script to remove QuietCMD from your PC.
+echo.
+pause
+echo. 
+
+echo Removing Key from Registry...
+reg delete "HKCU\Software\Microsoft\Command Processor" /v AutoRun ^
+  /t REG_EXPAND_SZ /d "%APPDATA%\QuietCMD\init.cmd" /f
+echo.
+
+echo Removing AppData Entry...
+rmdir %APPDATA%\QuietCMD\ /S /Q
+echo.
+
+echo Finished.
+pause
